ee929ff5569bb22b9b718375965e7c3c2bf65ae0
42doji <doji@student.42gyeongsan.kr>
2025-01-29 22:46:30 +0900

    parser first test

--- src/main.c
+++ src/main.c
@@ -12,19 +12,59 @@
 
 #include "../include/cub3d.h"
 
+
 int main(int argc, char *argv[])
 {
     t_game game;
 
     if (argc != 2)
     {
-        ft_putendl_fd("Usage: ./cub3D map.cub", 2);
-        return (EXIT_FAILURE);
+        ft_putendl_fd("Usage: ./cub3d <map.cub>", 2);
+        return (1);
     }
 
     if (!init_parser(&game, argv[1]))
-        return (EXIT_FAILURE);
+    {
+        ft_putendl_fd("Parsing failed.", 2);
+        return (1);
+    }
 
-    // 이후 게임 초기화 및 실행 로직
-    return (EXIT_SUCCESS);
+    // 파싱 결과 출력 (디버깅용)
+    printf("North Texture: %s\n", game.north_texture);
+    printf("South Texture: %s\n", game.south_texture);
+    printf("West Texture: %s\n", game.west_texture);
+    printf("East Texture: %s\n", game.east_texture);
+    printf("Floor Color: %d\n", game.floor_color);
+    printf("Ceiling Color: %d\n", game.ceiling_color);
+    printf("Map Rows: %d\n", game.map.rows);
+
+    // 맵 출력
+    for (int i = 0; i < game.map.rows; i++)
+    {
+        printf("%s\n", game.map.grid[i]);
+    }
+
+    cleanup_parser(&game);
+    return (0);
 }
+
+
+//
+//
+//
+//int main(int argc, char *argv[])
+//{
+//    t_game game;
+//
+//    if (argc != 2)
+//    {
+//        ft_putendl_fd("Usage: ./cub3D map.cub", 2);
+//        return (EXIT_FAILURE);
+//    }
+//
+//    if (!init_parser(&game, argv[1]))
+//        return (EXIT_FAILURE);
+//
+//    // 이후 게임 초기화 및 실행 로직
+//    return (EXIT_SUCCESS);
+//}
